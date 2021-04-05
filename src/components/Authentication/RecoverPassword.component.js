14d3670669c0e1f380f31c442b9d4419971983b2
Jada Pfeiffer <jadapfeiffer@Jadas-MacBook-Pro.local>
2021-04-05 10:03:53 -0400

    fixed css on  auth page'

--- src/components/Authentication/RecoverPassword.component.js
+++ src/components/Authentication/RecoverPassword.component.js
@@ -5,8 +5,8 @@ function RecoverPassword({setTab}){
   return <div>
       <h4>Recover Password</h4>
       <p>This is the Recover Password Page.</p>
-      <Link to="/" onClick={()=>{setTab('register')}}> Sign Up Now</Link>
-      <Link to="/" onClick={()=>{setTab('login')}}> Sign In Now</Link>
+      <Link to="/auth" onClick={()=>{setTab('register')}}> Sign Up Now</Link>
+      <Link to="/auth" onClick={()=>{setTab('login')}}> Sign In Now</Link>
     </div>
 };
  
