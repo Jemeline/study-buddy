d1941363d17a07df209d4aa200e39392142b9b02
Jada Pfeiffer <jadapfeiffer@Jadas-MacBook-Pro.local>
2021-04-20 14:11:20 -0500

    added matchlist to dash

--- src/components/Dashboard/StudentDashboard.component.js
+++ src/components/Dashboard/StudentDashboard.component.js
@@ -3,21 +3,22 @@ import {getIsSurveyed, getUser} from '../../utils/common';
 import DashboardUnsurveyed from './DashboardUnsurveyed.component';
 import Grid from '@material-ui/core/Grid';
 import {colorPalette} from '../../utils/design';
-import StudentUserList from './Student/StudentUserList.component';
+import StudentMatchList from './Student/StudentMatchList.component';
  
 function StudentDashboard() {
     return <div style={{backgroundColor:colorPalette.gray,zIndex:-1,height:'calc(100vh - 65px)',display:'flex',justifyContent:'center',alignItems: 'center',backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position:'fixed',width:'100vw',overflow:'auto'}}>
       {getIsSurveyed() ?
-      <Grid
-          container
-          direction="row"
-          justify="flex-start"
-          spacing={1}
-      >
-        <Grid xs={12} sm={6} md={3}>
+      // <Grid
+      //     container
+      //     direction="row"
+      //     justify="flex-start"
+      //     spacing={1}
+      // >
+      //   <Grid xs={12} sm={6} md={3}>
           
-        </Grid>
-      </Grid>  
+      //   </Grid>
+      // </Grid>  
+      <StudentMatchList/>
       : <DashboardUnsurveyed/>}
     </div>
 };
