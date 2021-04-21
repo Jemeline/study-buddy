d8ea5848bc2586671e11cfc78d25a7d152fae006
Jada Pfeiffer <jadapfeiffer@Jadas-MacBook-Pro.local>
2021-04-21 10:06:48 -0500

    added filtering to userlist

--- src/components/Dashboard/StudentDashboard.component.js
+++ src/components/Dashboard/StudentDashboard.component.js
@@ -8,17 +8,16 @@ import StudentMatchList from './Student/StudentMatchList.component';
 function StudentDashboard() {
     return <div style={{backgroundColor:colorPalette.gray,zIndex:-1,height:'calc(100vh - 65px)',display:'flex',justifyContent:'center',alignItems: 'center',backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position:'fixed',width:'100vw',overflow:'auto'}}>
       {getIsSurveyed() ?
-      // <Grid
-      //     container
-      //     direction="row"
-      //     justify="flex-start"
-      //     spacing={1}
-      // >
-      //   <Grid xs={12} sm={6} md={3}>
+      <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          spacing={1}
+      >
+        <Grid xs={12} sm={6} md={3}>
           
-      //   </Grid>
-      // </Grid>  
-      <StudentMatchList/>
+        </Grid>
+      </Grid>
       : <DashboardUnsurveyed/>}
     </div>
 };
