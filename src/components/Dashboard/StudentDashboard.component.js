9e1d44efd8458c1120196ff2df87802fb9cef9a3
rsievers82 <rsievers@live.unc.edu>
2021-04-21 22:26:49 -0400

    Update top matches and add to dashboard

--- src/components/Dashboard/StudentDashboard.component.js
+++ src/components/Dashboard/StudentDashboard.component.js
@@ -4,6 +4,7 @@ import DashboardUnsurveyed from './DashboardUnsurveyed.component';
 import Grid from '@material-ui/core/Grid';
 import {colorPalette} from '../../utils/design';
 import StudentMatchList from './Student/StudentMatchList.component';
+
  
 function StudentDashboard() {
     return <div style={{backgroundColor:colorPalette.gray,zIndex:-1,height:'calc(100vh - 65px)',display:'flex',justifyContent:'center',alignItems: 'center',backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position:'fixed',width:'100vw',overflow:'auto'}}>
@@ -11,12 +12,12 @@ function StudentDashboard() {
       <Grid
           container
           direction="row"
-          justify="flex-start"
+          justify="center"
           spacing={1}
       >
-        <Grid xs={12} sm={6} md={3}>
-          
-        </Grid>
+          <Grid item  xs="auto" sm="auto" md="auto">
+            <StudentMatchList />
+          </Grid>
       </Grid>
       : <DashboardUnsurveyed/>}
     </div>
