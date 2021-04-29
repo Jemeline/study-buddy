ba113c8df82b7fdc517233da89f8b2f9f0a2ea45
Clayton Saunders <claytonsaunders@Claytons-MBP.lan>
2021-04-29 10:10:44 -0400

    started the homepage

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
+import {Button,Col,Container,Form,FormGroup,FormFeedback,Input} from 'reactstrap';
+import logo from './study-buddy-tagline.png';
+import books from './dark-background-1.jpeg'
 import {Link} from 'react-router-dom';
 
 function Home({}){
-  return <div>
+  return <Container>
+    <Col style={{backgroundImage:books}}>
+    </Col>
+    <Col xs={6}>
+      <img src={logo} alt="logo" style={{height: '35vh'}}/>
       <h4>Home</h4>
       <p>This is the Home Page.</p>
       <Link to="/auth">Login/Register</Link>
-    </div>
+    </Col>
+    <Col></Col>
+      
+    </Container>
 };
  
 export default Home;
