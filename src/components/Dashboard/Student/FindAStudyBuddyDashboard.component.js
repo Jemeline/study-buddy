38c8a2ce9c75956b1c7cbb11cf044f4c6e211fc4
Jada Pfeiffer <jadapfeiffer@Jadas-MacBook-Pro.local>
2021-04-30 15:34:29 -0400

    added find a study buddy to dash

---
+++ src/components/Dashboard/Student/FindAStudyBuddyDashboard.component.js
@@ -0,0 +1,85 @@
+import React, { useState,useEffect } from "react";
+import { getUser, capitalizeFirst } from "../../../utils/common";
+import {apiGetStudents,apiGetStudentProfiles} from "../../../utils/api";
+import {colorPalette} from "../../../utils/design";
+import avatarUnknown from '../../Profile/Student/unknown-avatar.jpg';
+import Paper from '@material-ui/core/Paper';
+import { useHistory } from "react-router-dom";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import ReactLoading from "react-loading";
+
+function FindAStudyBuddyDashboard() {
+    const history = useHistory();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [user, setUser] = useState(JSON.parse(getUser()));
+    const [profile, setProfile] = useState({});
+    const [rows, setRows] = useState([]);
+    const [hiddenTable,setHiddenTable] = useState(false);
+    const [hiddenProfile,setHiddenProfile] = useState(true);
+    const [hideProfileTabs, setHideProfileTabs] = useState(false);
+    
+    useEffect(async () => {
+        try{
+          setLoading(true);
+          setError(false);
+          const data = await apiGetStudents();
+          const data1 = await apiGetStudentProfiles();
+          if (data.data != null) {
+            setRows(await data.data.map((e)=> createData((capitalizeFirst(e.first) + ' '+ capitalizeFirst(e.last)),e.email,e.phoneNumber,e,data1.data.find(element => element._userId === e._id))));
+            setLoading(false);
+          } else {
+            setError(true);
+          }  
+        } catch (err){
+          setError(true);
+        }  
+    }, []);
+
+    return (
+        <div>
+            {loading ? <div style={{backgroundColor:'white',zIndex:-1,height:'400px',display:'flex',justifyContent:'center',alignItems: 'center',width:'100%',overflow:'auto'}}><ReactLoading height={'20%'} width={'20%'} type={"cylon"} color={colorPalette.secondary}/></div>:
+            <Paper hidden={hiddenTable} style={{overflow:'auto',width:"100%",height:'400px',margin:'auto',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',cursor:'pointer'}} onClick={()=>history.push('/find-students')}>
+                <TableContainer>
+                    <Table stickyHeader size="medium">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell colspan="4" style={{ "text-align": "center",fontSize:'3vh',fontFamily: 'Garamond, serif' }}><strong>Browse Users</strong></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left" width='5vw'></TableCell>
+                            <TableCell align="left" width='5vw'>Name</TableCell>
+                            <TableCell align="left" width='7vw'>Email</TableCell>
+                            <TableCell align="left">Program of Study</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {!loading ? 
+                        rows.filter((row)=>!((row.user._id ===JSON.parse(getUser())._id) || row.user.disabled)).map((row) => (
+                        <TableRow hover key={row.user._id}>
+                            <TableCell onClick={()=> {setUser(row.user);setProfile(row.profile);setHiddenTable(true);setHiddenProfile(false);}} align="left">{(!row.user.avatar)?<img src={avatarUnknown} style={{height: '5vw',width:"5vw",borderRadius:'50%'}}/>:<div style={{borderRadius:'50%',height: '5vw',width:"5vw",backgroundImage:`url(${row.user.avatar})`,backgroundSize:'cover',backgroundPosition:'center'}}/>}</TableCell>
+                            <TableCell onClick={()=> {setUser(row.user);setProfile(row.profile);setHiddenTable(true);setHiddenProfile(false);}} align="left">{row.name}</TableCell>
+                            <TableCell onClick={()=> {setUser(row.user);setProfile(row.profile);setHiddenTable(true);setHiddenProfile(false);}} align="left">{row.email}</TableCell>
+                            <TableCell onClick={()=> {setUser(row.user);setProfile(row.profile);setHiddenTable(true);setHiddenProfile(false);}} align="left">{(typeof(row.profile)==='undefined')? '':(row.profile.studentType === 'undergraduate')?row.profile.programOfStudy.major:row.profile.programOfStudy.graduateProgram[0]}</TableCell>
+                        </TableRow>
+                        )): <TableRow/>}
+                    </TableBody>
+                    </Table>
+                </TableContainer> 
+            </Paper>} 
+        </div>
+    );
+}
+
+function createData(name, email, phone, user,profile) {
+    return { name, email, phone, user, profile};
+};
+
+export default FindAStudyBuddyDashboard;
--- src/components/Dashboard/StudentDashboard.component.js
+++ src/components/Dashboard/StudentDashboard.component.js
@@ -5,9 +5,11 @@ import Grid from '@material-ui/core/Grid';
 import {colorPalette} from '../../utils/design';
 import ProfileDashboard from './Student/ProfileDashboard.component';
 import InviteDashboard from './Student/InviteDashboard.component';
+import FindAStudyBuddyDashboard from './Student/FindAStudyBuddyDashboard.component';
 
  
 function StudentDashboard() {
+    
     return <div style={{backgroundColor:colorPalette.gray,zIndex:-1,height:'calc(100vh - 65px)',display:'flex',justifyContent:'center',alignItems: 'center',backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover',width:'100vw',overflow:'auto'}}>
       {getIsSurveyed() ?
       <div style={{flexGrow: 1,height:'calc(100vh - 95px)',overflowY:'auto',overflowX:'hidden', margin:'20px'}}>
@@ -15,7 +17,7 @@ function StudentDashboard() {
           container
           direction="row"
           justify="center"
-          spacing={1}
+          spacing={3}
       >
           <Grid item xs={3}>
               <InviteDashboard/>
@@ -25,14 +27,18 @@ function StudentDashboard() {
           <Grid item xs={4}>
             <ProfileDashboard/>
           </Grid>
-          <Grid item xs={4}>
+
+          <Grid item xs={5}>
+            <FindAStudyBuddyDashboard/>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={7}>
           </Grid>
-          <Grid item xs={4}>
+
+          <Grid item xs={6}>
           </Grid>
           <Grid item xs={6}>
           </Grid>
+          
           <Grid item xs={6}>
           </Grid>
           <Grid item xs={6}>
